f25ee671b52cb9a3d128905ac4a312ae22699779
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2023-07-19 00:30:06 +0200

    Add functionality for searching target term

--- linguistlab.py
+++ linguistlab.py
@@ -33,7 +33,11 @@ class Glossary:
         return search_results
 
     def search_target_term(self, search_term: str) -> Dict[str, List[str]]:
-        pass
+        search_results = {}
+        for source_term in self.glossary_content.keys():
+            if search_term in self.glossary_content[source_term]:
+                search_results[source_term] = self.glossary_content[source_term]
+        return search_results
 
     # TODO
     # def add_source_term(self):
